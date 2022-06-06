28ee4652ab5ee4e07386f50241b2b84867a4f842
ajedrych <jedrych.al@gmail.com>
2022-06-06 11:26:47 +0200

    update arima for non seasoanal time series

--- arima, sarima.R
+++ arima, sarima.R
@@ -8,6 +8,7 @@ par(mfrow=c(1,1))
 
 ################################################################################
 ##################### DANE NIESEZOWNOWE - REALNY KURS WALUTOWY POLSKI ##########
+################################################################################
 df <- read_excel("niesez.xls")
 
 class(df)
@@ -17,14 +18,22 @@ is.ts(df)
 df = ts(data=df$Value, frequency = 12,             
         start=c(1994,1), end=c(2021,12)) 
 
+df.in <- 
+  window(df,
+         end = c(2021, 09))
+
+df.out <- 
+  window(df,
+         start = c(2021, 10))
+
 # test Ljung-Box
-Box.test(df, lag=24, type="Ljung-Box") #odrzucamy H0: df to biały szum, bo p-value <0.05
+Box.test(df.in, lag=24, type="Ljung-Box") #odrzucamy H0: df to biały szum, bo p-value <0.05
 
 # test Box-Pierce
-Box.test(df, lag=24, type="Box-Pierce") #odrzucamy H0: df to biały szum, bo p-value <0.05
+Box.test(df.in, lag=24, type="Box-Pierce") #odrzucamy H0: df to biały szum, bo p-value <0.05
 
 # wykres
-plot(df,
+plot(df.in,
      type = "l",
      col  = "blue",
      lwd  = 2,
@@ -32,7 +41,7 @@ plot(df,
 
 # pierwsze różnice
 library(xts)
-diff.df<-diff.xts(df)
+diff.df.in<-diff.xts(df.in)
 
 par(mfrow=c(1,2))
 
@@ -43,40 +52,52 @@ plot(diff.df,
      main = "Pierwsze różnice")
 
 #logarytmowanie w celu wyeliminowania zmieniającej się wariancji
-ln.df<-log(df)
-diff.ln.df<-diff.xts(ln.df)
+ln.df.in<-log(df.in)
+diff.ln.df.in<-diff.xts(ln.df.in)
 
-plot(diff.ln.df,
+plot(diff.ln.df.in,
      type = "l",
      col  = "blue",
      lwd  = 2,
      main = "Pierwsze różnice dla logarytmu")
 
-# test dickeya-fullera, pierwsza forma funkcyjna (non drift)
+# test dickeya-fullera, druga forma funkcyjna (drift)
 install.packages("fUnitRoots")
 library(fUnitRoots)
 source("funs/TESTDF.R") 
 
-testdf(variable = ln.df, ADF_type="nc", ADF_max_order = 5,BG_max_order = 13)
+testdf(variable = ln.df.in, ADF_type="c", ADF_max_order = 5,BG_max_order = 6)
 # dla testow Breuscha-Godfreya p-value <0.05 => odrzucamy H0 o braku autokorelacji (augmentations = 0)
-# przechodzimy do testu ADF z jednym rozszerzeniem (augmentations = 1): sla testów BG p-value >0,05 => brak podstaw do odrzucenia
+# przechodzimy do testu ADF z jednym rozszerzeniem (augmentations = 1): dla testów BG p-value >0,05 => brak podstaw do odrzucenia
 # H0 o braku autokorelacji
-# patrzymy na p-value dla ADF = 0.84 => brak podstaw do odrzucenia H0 o tym, że ln.df jest zmienną niestacjonarną
-# przeprowadzamy test dla pierwszych różnic
+# patrzymy na p-value dla ADF = 0.4387209 < 0,05 => odrzucamy H0 o tym, że ln.df jest zmienna niestacjonarna
 
-testdf(variable = diff.ln.df, ADF_type="nc", ADF_max_order = 5,BG_max_order = 13)
-# dla testów B-G p-value >0,05 => brak podstaw do odrzucenia H0 o braku autokorelacji (augmentations=0)
-# patrzymy na p-valu D-F = 0,01 <0.05 => odrzucamy h0 o tym, że diff.ln.df jest zmienna niestacjonarna
+testdf(variable = diff.ln.df.in, ADF_type="c", ADF_max_order = 5,BG_max_order = 6)
+# augmentations = 0 => dla testów B-G p-value > 0.05 => brak podstaw do odrzucenia h0 o braku autokorelacji
+# p-value dla testu D-F 0.01 < 0.05 => odrzucamy H0 o tym, że ln.df jest zmienną nieatacjonarna
+
 
 # test KPSS
-df.kpss.test <- ur.kpss(ln.df, type = c("mu"))  # stała w równaniu testowym
-summary(df.kpss.test)
-# statystyka testowa (2,449) > 0,463 => odrzucamy H0 o stacjonarnosci ln.df
+library(urca)
+
+df.in.kpss.test <- ur.kpss(ln.df.in, type = c("mu"))  # stała w równaniu testowym
+summary(df.in.kpss.test)
+# statystyka testowa (2,4579) > 0,463 => odrzucamy H0 o stacjonarnosci ln.df
 # przeprowadzamy test dla pierwszych różnic
 
-diff.df.kpss.test <- ur.kpss(diff.ln.df, type = c("mu"))  # stała w równaniu testowym
-summary(diff.df.kpss.test)
+diff.df.in.kpss.test <- ur.kpss(diff.ln.df.in, type = c("mu"))  # stała w równaniu testowym
+summary(diff.df.in.kpss.test)
 # statystyka testowa = 0.1635 < 0,463 => brak podstaw do odrzucenia h0 o stacjonarnosci diff.ln.df
+
+# test Ljung-Box
+Box.test(diff.ln.df.in, lag=24, type="Ljung-Box") #brak podstaw do odrzucenia H0 o tym, że diff.ln.df.in to bialy szum, poniewaz p-value>0.05 
+
+# test Box-Pierce
+Box.test(diff.ln.df.in, lag=24, type="Box-Pierce") #brak podstaw do odrzucenia H0 o tym, że diff.ln.df.in to bialy szum, poniewaz p-value>0.05 
+
+
+library(forecast)
+tsdisplay(ln.df, lag.max=24)
 
 par(mfrow = c(1, 2))
 acf(na.omit(diff.ln.df), lag.max = 36,
